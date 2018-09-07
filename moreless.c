2ed9c290e6fe09bbe8e81a34a65eb9c14334738c
P3aro <33288695+P3aro@users.noreply.github.com>
2018-09-07 13:35:56 +0200

    Create moreless.c

---
+++ moreless.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int rand(void);
+
+void    ft_moreless(int nbr)
+{
+    int nbr = 0;
+    int i = 0;
+    int mystere = 0
+    for (i = 0, i >=0 && i <= 100, i++)
+    {
+        mystere = rand ();
+    while (nbr != i)
+    {
+        scanf("%d", &nbr)
+        if (nbr < i)
+            printf("%s", "c'est plus!");
+        else
+            printf("%s", "c'est moins!");
+    }
+    printf("%s", "Bravo, vous avez trouve le nombre mystere !!!");
+}
+
+int main()
+{
+    ft_moreless();
+    return (0);
+}
